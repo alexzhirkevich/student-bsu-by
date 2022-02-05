3b69b38b7407346a8e01891cd3d7794aec9faea6
Aleksandr <63979218+alexzhirkevich@users.noreply.github.com>
2022-02-05 23:42:44 +0300

    delete build

--- app/build/tmp/kapt3/stubs/release/github/alexzhirkevich/studentbsuby/workers/SyncronizationWorkerKt.java
+++
@@ -1,8 +0,0 @@
-package github.alexzhirkevich.studentbsuby.workers;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 5, 1}, k = 2, d1 = {"\u0000\b\n\u0000\n\u0002\u0010\b\n\u0000\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0002"}, d2 = {"HostelUpdateNotification", "", "app_release"})
-public final class SyncronizationWorkerKt {
-    private static final int HostelUpdateNotification = 494376142;
-}
